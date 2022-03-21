204dd3bfed555dd538501243ac66e455ac6ebb80
Heath Paddock <hp@heathpaddock.com>
2022-03-20 20:31:48 -0500

    Scaffold a few components and add tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BrowserComponent } from './browser/browser.component';
+import { SetupGuard } from './setup.guard';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: BrowserComponent }
+  { path: '', component: WelcomeComponent, canActivate: [SetupGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserComponent } from './browser/browser.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrowserComponent
+    BrowserComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
